9707a7a53993676a5236b27cf761dc473864a391
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-08 08:25:08 +0500

    Create server.js

---
+++ class 30/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const users = [];
+app.use(express.json());
+
+app.post('/users',(req,res)=>{
+    console.log(req.body);
+    const { name } = req.body;
+    if(!name || name.length < 3){
+        return res.status(400).send("Invalid  name");
+    }
+    users.push(name);
+    res.status(200).send(`${name} added in users successfully`);
+})
+
+app.get('/users',(req,res)=>{
+    res.status(200).send({users});
+})
+
+app.get('/users/:userid',(req,res)=>{
+    const { userid } = req.params;
+    const result = users[userid];
+    if(!result){
+      return  res.status(400).send("invalid userid")
+    }
+    res.status(200).send({user :result})
+})
+
+app.put('/users/:userid',(req,res)=>{
+    const { name } = req.body;
+    const { userid } = req.params;
+    const result = users[userid];
+
+    if(!result){
+        res.status(400).send("invalid userid")
+    }
+    users[userid]=name;
+    res.status(200).send({ users})
+})
+
+app.delete('/users/:userid',(req,res)=>{
+    const {userid} = req.params;
+    const result = users[userid];
+    if(!result){
+        res.status(400).send("invalid userid")
+    }
+    users.splice(userid,1);
+    res.status(200).send({users})
+})
+
+
+
+
+app.listen(4000)
