8adace1b33c36db6a37cb1129aaf40366e68a9d4
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-09 21:56:11 +0500

    Create student.js

---
+++ class 35/models/student.js
@@ -0,0 +1,32 @@
+const sequelize = require("./index");
+const { DataTypes} = require('sequelize');
+
+const tableName = "students";
+
+const Student = sequelize.define(tableName , {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    createdAt:{
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.CURRENT_TIMESTAMP
+    },
+    updatedAt:{
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.CURRENT_TIMESTAMP
+    },
+});
+
+
+module.exports = Student;
