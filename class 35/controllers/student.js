dc47111a32f19b6e2c67c6d6dcd1c6b014bdb381
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-09 21:56:04 +0500

    Create student.js

---
+++ class 35/controllers/student.js
@@ -0,0 +1,35 @@
+const Students = require("../models/student");
+
+async function post(req,res){
+    try{
+        const { name, email, password} = req.body;
+        if(!name || !email) return res.status(400).send("required fields cannot be empty");
+        const emailFound = await Students.findOne({
+            where: {email:email}
+        });
+
+        if(emailFound) return res.status(400).send("email already exists");
+
+       const Student = await Students.create({
+        name:name,
+        email:email
+       });
+        res.status(200).send({Student});
+    }catch (err){
+        console.log(err);
+        res.stauts(500).send("something went wrong")    
+    }
+}
+async function getall(req,res){
+    try{
+        const students = await Students.findAll()
+        res.status(200).send({students}); 
+    }catch(err){
+        console.log(err);
+        res.stauts(500).send("something went wrong")   
+    }
+}
+module.exports = {
+    post,
+    getall
+}
