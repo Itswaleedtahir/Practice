9b5c84b54c3ead9429320b391e35cc0e4a644913
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-16 21:03:44 +0500

    Update server.js

--- class 33/server.js
+++ class 33/server.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose')
 const app = express();
 app.use(express.json());
 
+
+//controller Route
 const studentRouter = require('./routes/student');
 
 app.use('/api/students',studentRouter);
+
+
 
 mongoose.connect('mongodb+srv://itswaleedtahir:itswaleedtahir123@cluster0.smf6v.mongodb.net/DCSE').then(result=>{
     console.log("database connected")
